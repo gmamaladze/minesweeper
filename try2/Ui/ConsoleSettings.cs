20f34cf84118ffe6c46b6dca81ffdc103fa8bc5a
unknown <admage7@MD1HJ4SC.ww004.siemens.net>
2016-01-07 22:57:39 +0100

    Fixded gitignore. Added accidentally untracked files.

---
+++ try2/Ui/ConsoleSettings.cs
@@ -0,0 +1,55 @@
+// // This code is distributed under MIT license. 
+// // Copyright (c) 2015-2016 George Mamaladze
+// // See license.txt or http://opensource.org/licenses/mit-license.php
+
+using System;
+using Fmines.Geometry;
+
+namespace Fmines.Ui
+{
+    internal class ConsoleSettings
+    {
+        private readonly ConsoleColor _backgroundColor;
+        private readonly int _cursorSize;
+        private readonly ConsoleColor _foregroundColor;
+        private readonly Size _windowSize;
+
+        private ConsoleSettings(
+            int cursorSize,
+            Size windowSize,
+            ConsoleColor backgroundColor,
+            ConsoleColor foregroundColor)
+        {
+            _cursorSize = cursorSize;
+            _windowSize = windowSize;
+            _backgroundColor = backgroundColor;
+            _foregroundColor = foregroundColor;
+            Console.CancelKeyPress += CancelKeyPressed;
+        }
+
+        public static ConsoleSettings Capture()
+        {
+            return new ConsoleSettings(
+                Console.CursorSize,
+                new Size(Console.WindowWidth, Console.WindowHeight),
+                Console.BackgroundColor,
+                Console.ForegroundColor);
+        }
+
+        private void CancelKeyPressed(object sender, ConsoleCancelEventArgs e)
+        {
+            Restore();
+        }
+
+
+        public void Restore()
+        {
+            Console.ForegroundColor = _foregroundColor;
+            Console.BackgroundColor = _backgroundColor;
+            Console.CursorSize = _cursorSize;
+            Console.SetWindowSize(_windowSize.Width, _windowSize.Height);
+            Console.Clear();
+            Console.CancelKeyPress -= CancelKeyPressed;
+        }
+    }
+}
